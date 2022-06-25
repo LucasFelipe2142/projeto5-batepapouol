5ce59c87514c8bed780711cff25cea2984a0f81d
Lucas <lucasfelicio378@gmail.com>
2022-06-25 12:29:16 -0300

    recebe as mensagens do servidor

--- scripts.js
+++ scripts.js
@@ -1,22 +1,71 @@
 let msg_container = document.querySelector('.container-msg');
 let name;
+let nome1;
+
 function iniciar(){
-    const date = new Date().toLocaleTimeString();
     name = prompt("Digite o seu nome; ");
-    msg_container.innerHTML += `
-                                <div class="msg-entrou">
+    const nome = {
+        name: name
+    };
+    nome1 = nome;
+    const promise = axios.post(
+        "https://mock-api.driven.com.br/api/v6/uol/participants",
+        nome
+      );
+    promise.then(inserir_nome);
+    promise.catch(deu_erro);
+}
+iniciar();
+
+function deu_erro(error){
+    alert(`erro ${error.response.status}, insira outro nome`)
+    iniciar();
+    
+}
+
+function inserir_nome(){
+    console.log("foi")
+}
+
+
+function buscar_msg(){
+ 
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    promise.then(busca)
+}
+buscar_msg()
+
+function busca (busca){
+    console.log(busca.data[99])
+    const array = busca.data;
+
+    for (let i = 0; i < array.length; i++) {
+        msg_container.innerHTML += `
+                                <div class="msg">
                                     <div class="hora">
-                                        (${date})
+                                        (${array[i].time})
                                     </div>
 
                                     <div class="name">
-                                        ${name}
+                                        ${array[i].from}
+                                    </div>
+                                    
+                                    para
+
+                                    <div class ="privacidade">
+                                        ${array[i].type}
                                     </div>
 
-                                    entrou na sala
-                                </div> `;
+                                        ${array[i].text}
+                                 </div> `;
+        
+    }
+
+    
+        
+    
+
 }
-iniciar();
 
 
 function enviar_msg(){
@@ -40,4 +89,18 @@ function enviar_msg(){
 
                                     ${msg}
                                 </div> `;
+
+    const elementoQueQueroQueApareca = document.querySelector('.msg');
+    elementoQueQueroQueApareca.scrollIntoView();
+
+    function logado(){
+        const promise = axios.post(
+            "https://mock-api.driven.com.br/api/v6/uol/status",
+            nome1
+          );
+        promise.then(inserir_nome);
+        promise.catch(deu_erro);
+    }
+
+    setInterval(logado, 5000);
 }
